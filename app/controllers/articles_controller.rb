5a28c332abbc77f06e779fcc05109b56a4769d96
Gary Taylor <gary.taylor@hismessages.com>
2015-04-26 21:36:43 +0100

    New articles can now be created.  However, I have disabled CSRF checking as I have not implemented anything in the API to get this token.  I would normally do this as part of a login procedure if the app required login.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,28 @@
 #
 # The controller used to access the articles resources
 class ArticlesController < ApplicationController
+  #@TODO This is a workaround as I havent implemented a way to get the authenticity token over the API
+  protect_from_forgery except: :create
   #
   # Route: GET /articles
   # Returns all articles as JSON
   def index
     @articles = Article.all
+  end
+  def create
+    @article = Article.new params.permit(:title, :body, :url_slug)
+    @article.owner = current_user
+    @article.body_content_type = 'text/x-markdown'
+    @article.posted_on = Time.now
+    if @article.save
+      respond_to do |format|
+        format.json
+      end
+    else
+      respond_to do |format|
+        render :error
+      end
+    end
   end
 
 end
---
+++ app/views/articles/create.json.jbuilder
@@ -0,0 +1,14 @@
+json.author @article.owner.name
+json.title @article.title
+json.posted_on @article.posted_on
+json.body @article.body
+json.body_content_type @article.body_content_type
+json.id @article.id
+json.url_slug @article.url_slug
+json.comments do
+  json.array! @article.comments do |comment|
+    json.body comment.body
+    json.author comment.owner.name
+    json.posted_on comment.posted_on
+  end
+end
