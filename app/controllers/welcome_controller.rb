452fbacd207a22742dbc45b95f45545888da0764
Gary Taylor <gary.taylor@hismessages.com>
2015-04-26 16:01:03 +0100

    Added welcome controller to provide an entry point into the app Added welcome view and layout to load an ExtJS app

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+# A controller for welcoming the user - giving them somewhere to go !!
+class WelcomeController < ApplicationController
+  #The main welcome page - nothing needs doing in the controller
+  def index
+
+  end
+
+end
